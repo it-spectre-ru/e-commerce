01b2e7e8e22517b2f06ee1d6c27081c44fbcdc45
tsvetkovpro <285019@gmail.com>
2018-02-25 19:37:51 +0500

    signup part 3

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var ejsMate = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+
 
 
 var User = require('./models/user');
@@ -24,6 +28,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: "123abc"
+}));
+app.use(flash());
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 
