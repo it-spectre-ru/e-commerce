4a2d467737a5299b35df25d18d7312d3725da5fe
it-spectre-ru <it.spectre.ru@gmail.com>
2018-02-26 21:33:36 +0500

    login part 1

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var ejsMate = require('ejs-mate');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 
 
 var secret = require('./config/secret');
@@ -32,7 +34,8 @@ app.use(cookieParser());
 app.use(session({
   resave: true,
   saveUninitialized: true,
-  secret: secret.secretKey
+  secret: secret.secretKey,
+  store: new MongoStore({url: secret.database, autoReconnect: true})
 }));
 app.use(flash());
 app.engine('ejs', ejsMate);
