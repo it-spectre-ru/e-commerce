df0c2f56849c0f3ba38fe9a5616178091621a4b2
it-spectre-ru <it.spectre.ru@gmail.com>
2018-02-26 22:02:15 +0500

    login part 2

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.use(session({
   store: new MongoStore({url: secret.database, autoReconnect: true})
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 
