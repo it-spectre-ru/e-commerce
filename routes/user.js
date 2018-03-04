7141e14bcf959acb48e0c16539fcce174d68beb9
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-04 22:03:14 +0500

    fixing some codes - 1

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ var User = require('../models/user');
 var passport = require('passport');
 var passportConf = require('../config/passport');
 
-
 router.get('/login', function(req, res) {
   if (req.user) return res.redirect('/');
   res.render('accounts/login', {message: req.flash('loginMessage')});
@@ -12,21 +11,22 @@ router.get('/login', function(req, res) {
 router.post('/login', passport.authenticate('local-login', {
   successRedirect: '/profile',
   failureRedirect: '/login',
-  failureFlash: true
+  failureFlash: true,
 }));
 
 router.get('/profile', function(req, res, next) {
   User.findOne({_id: req.user._id}, function(err, user) {
     if (err) return next(err);
-    res.render('accounts/profile', {user: user});
-  });
 
+    res.render('accounts/profile', {user: user});
+
+  });
 
 });
 
 router.get('/signup', function(req, res, next) {
   res.render('accounts/signup', {
-    errors: req.flash('errors')
+    errors: req.flash('errors'),
   });
 });
 
@@ -40,16 +40,20 @@ router.post('/signup', function(req, res, next) {
   User.findOne({email: req.body.email}, function(err, existingUser) {
     if (existingUser) {
       req.flash('errors', 'Account with that email adress already exists');
-      return res.redirect('/signup')
+      return res.redirect('/signup');
     } else {
       user.save(function(err, user) {
         if (err) return next(err);
 
         return res.redirect('/');
-      })
+      });
     }
   });
 });
 
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
