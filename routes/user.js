ff0c9ef4e400ae38788003bf9068dd2c3d53c283
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-09 19:48:50 +0500

    modify signup route

--- routes/user.js
+++ routes/user.js
@@ -1,65 +1,75 @@
 var router = require('express').Router();
 var User = require('../models/user');
-var faker = require('faker');
+var Cart = require('../models/cart');
+var async = require('async');
 var passport = require('passport');
 var passportConf = require('../config/passport');
 
-
 router.get('/login', function(req, res) {
   if (req.user) return res.redirect('/');
-  res.render('accounts/login', { message: req.flash('loginMessage')});
+  res.render('accounts/login', {message: req.flash('loginMessage')});
 });
 
 router.post('/login', passport.authenticate('local-login', {
   successRedirect: '/profile',
   failureRedirect: '/login',
-  failureFlash: true
+  failureFlash: true,
 }));
 
 router.get('/profile', function(req, res, next) {
-  User.findOne({ _id: req.user._id }, function(err, user) {
+  User.findOne({_id: req.user._id}, function(err, user) {
     if (err) return next(err);
 
-    res.render('accounts/profile', { user: user });
+    res.render('accounts/profile', {user: user});
 
   });
-
 
 });
 
 router.get('/signup', function(req, res, next) {
   res.render('accounts/signup', {
-    errors: req.flash('errors')
+    errors: req.flash('errors'),
   });
 });
 
 router.post('/signup', function(req, res, next) {
-  var user = new User();
 
-  user.profile.name = req.body.name;
-  user.email = req.body.email;
-  user.password = req.body.password;
-  user.profile.picture = faker.image.avatar();
+  async.waterfall([
+    function(callback) {
+      var user = new User();
 
-  User.findOne({ email: req.body.email }, function(err, existingUser) {
+      user.profile.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
+      user.profile.picture = user.gravatar();
 
-    if (existingUser) {
-      req.flash('errors', 'Account with that email address already exists');
-      return res.redirect('/signup');
-    } else {
-      user.save(function(err, user) {
+      User.findOne({email: req.body.email}, function(err, existingUser) {
+
+        if (existingUser) {
+          req.flash('errors', 'Account with that email address already exists');
+          return res.redirect('/signup');
+        } else {
+          user.save(function(err, user) {
+            if (err) return next(err);
+            callback(null, user);
+          });
+        }
+      });
+    },
+
+    function(user) {
+      var cart = new Cart();
+      cart.owner = user._id;
+      cart.save(function(err) {
         if (err) return next(err);
-
         req.logIn(user, function(err) {
           if (err) return next(err);
           res.redirect('/profile');
-
-        })
+        });
       });
     }
-  });
+  ]);
 });
-
 
 router.get('/logout', function(req, res, next) {
   req.logout();
@@ -67,11 +77,11 @@ router.get('/logout', function(req, res, next) {
 });
 
 router.get('/edit-profile', function(req, res, next) {
-  res.render('accounts/edit-profile', { message: req.flash('success')});
+  res.render('accounts/edit-profile', {message: req.flash('success')});
 });
 
 router.post('/edit-profile', function(req, res, next) {
-  User.findOne({ _id: req.user._id }, function(err, user) {
+  User.findOne({_id: req.user._id}, function(err, user) {
 
     if (err) return next(err);
 
