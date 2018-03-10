73ec8a7a26d218c5eed05204a8eb99da9332c847
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-10 08:58:46 +0500

    adding product to a cart Route

--- routes/main.js
+++ routes/main.js
@@ -7,21 +7,20 @@ function paginate(req, res, next) {
   var perPage = 9;
   var page = req.params.page;
 
-  Product
-    .find()
-    .skip( perPage * page)
-    .limit( perPage )
-    .populate('category')
-    .exec(function(err, products) {
-      if (err) return next(err);
-      Product.count().exec(function(err, count) {
+  Product.find().
+      skip(perPage * page).
+      limit(perPage).
+      populate('category').
+      exec(function(err, products) {
         if (err) return next(err);
-        res.render('main/product-main', {
-          products: products,
-          pages: count / perPage
+        Product.count().exec(function(err, count) {
+          if (err) return next(err);
+          res.render('main/product-main', {
+            products: products,
+            pages: count / perPage,
+          });
         });
       });
-    });
 
 }
 
@@ -50,6 +49,22 @@ stream.on('error', function(err) {
   console.log(err);
 });*/
 
+router.post('/product/:product_id', function(req, res, next) {
+  Cart.findOne({owner: req.user._id}, function(err, cart) {
+    cart.items.push({
+      item: req.body.product_id,
+      price: parseFloat(req.body.priceValue),
+      quantity: parseInt(req.body.quantity),
+    });
+
+    cart.total = (cart.total + parseFloat(req.body.priceValue)).toFixed(2);
+
+    cart.save(function(err) {
+      if (err) return next(err);
+      return res.redirect('/cart')
+    });
+  });
+});
 
 router.post('/search', function(req, res, next) {
   res.redirect('/search?q=' + req.body.q);
@@ -58,16 +73,16 @@ router.post('/search', function(req, res, next) {
 router.get('/search', function(req, res, next) {
   if (req.query.q) {
     Product.search({
-      query_string: { query: req.query.q}
+      query_string: {query: req.query.q},
     }, function(err, results) {
       results:
-      if (err) return next(err);
+          if (err) return next(err);
       var data = results.hits.hits.map(function(hit) {
         return hit;
       });
       res.render('main/search-result', {
         query: req.query.q,
-        data: data
+        data: data,
       });
     });
   }
@@ -84,7 +99,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/page/:page', function(req, res, next) {
-  paginate(req,res,next);
+  paginate(req, res, next);
 });
 
 router.get('/about', function(req, res) {
@@ -92,23 +107,21 @@ router.get('/about', function(req, res) {
 });
 
 router.get('/products/:id', function(req, res, next) {
-  Product
-    .find({ category: req.params.id })
-    .populate('category')
-    .exec(function(err, products) {
-      if (err) return next(err);
-      res.render('main/category', {
-        products: products
+  Product.find({category: req.params.id}).
+      populate('category').
+      exec(function(err, products) {
+        if (err) return next(err);
+        res.render('main/category', {
+          products: products,
+        });
       });
-    });
 });
 
-
 router.get('/product/:id', function(req, res, next) {
-  Product.findById({ _id: req.params.id }, function(err, product) {
+  Product.findById({_id: req.params.id}, function(err, product) {
     if (err) return next(err);
     res.render('main/product', {
-      product: product
+      product: product,
     });
   });
 });
