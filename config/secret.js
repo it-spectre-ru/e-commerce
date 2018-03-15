3ec2b4810b16ab961c173200e9a992c95467a17d
it-spectre-ru <it.spectre.ru@gmail.com>
2018-03-15 21:59:22 +0500

    fb login 1: config on Node.js side

--- config/secret.js
+++ config/secret.js
@@ -2,4 +2,11 @@ module.exports = {
   database: 'mongodb://root:toor@ds245218.mlab.com:45218/amazon-clone',
   port: 4000,
   secretKey: '123abc'
+
+  facebook: {
+    clientID: process.env.FACEBOOK_ID || '149839915711070',
+    clientSecret: process.env.FACEBOOK_SECRET || '18437c34f32e473447b3c5cd9e9bb2bd',
+    profileFields: ['emails', 'displayName'],
+    callbackURL: 'http://localhost:4000/auth/facebook/callback'
+  }
 }
